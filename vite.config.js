6b82161d17bb2cfb63e3ac98d098ed75d22dbd8b
yonardr <rustem2129@mail.ru>
2024-07-07 17:36:33 +0300

    load boost

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,77 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import externalGlobals from "rollup-plugin-external-globals";
+import compressPlugin from "vite-plugin-compression";
+import viteImagemin from 'vite-plugin-imagemin'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+
+  plugins: [vue(),
+    compressPlugin({
+      verbose: true,
+      disable: false,
+      deleteOriginFile: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 20
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
+    }),
+  ],
   build: {
-    ChunksizeWarninglimit: 1000, // Simple module file size limits 1000KB
+    chunkSizeWarningLimit: 1000,
     terserOptions: {
-      // Clear Console and Debugger in the code
       compress: {
         drop_console: true,
         drop_debugger: true,
       },
     },
     rollupOptions: {
+      // The following files are not packaged
+      external: [
+        "vue",
+        "element-plus",
+        "axios",
+        "vue-router",
+        "echarts",
+      ],
+      plugins: [
+        // When packing, automatically replace the introduction in the code to CDN to introduce
+        externalGlobals({
+          vue: "Vue",
+          "element-plus": "ElementPlus",
+          axios: "axios",
+          "vue-router": "VueRouter",
+          echarts: "echarts",
+        }),
+      ],
       output: {
         manualChunks(id) {
-          // Static resource split
           if (id.includes("node_modules")) {
             return id
                 .toString()
