27351d6b038244baead4fecd5ff2cf505ee5292e
yonardr <rustem2129@mail.ru>
2024-07-07 16:53:35 +0300

    load optimization

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,28 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    ChunksizeWarninglimit: 1000, // Simple module file size limits 1000KB
+    terserOptions: {
+      // Clear Console and Debugger in the code
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Static resource split
+          if (id.includes("node_modules")) {
+            return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+          }
+        },
+      },
+    },
+  },
+});
